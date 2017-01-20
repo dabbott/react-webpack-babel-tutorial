59d7a33b4462136686e44c625c33a09ceab023a4
Devin Abbott <devinabbott@gmail.com>
2017-01-19 17:36:10 -0800

    Bare minimum react, webpack, and babel setup

---
+++ index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const styles = {
+  app: {
+    paddingTop: 40,
+    textAlign: 'center',
+  },
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div style={styles.app}>
+        Welcome to React!
+      </div>
+    )
+  }
+}
+
+const root = document.querySelector('#app')
+ReactDOM.render(<App />, root)
